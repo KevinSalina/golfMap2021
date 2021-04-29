739eb0128b3cbed6ef51842539ebb608be62e314
Kevin Salina <kevin.salina25@gmail.com>
2021-04-29 13:11:38 -0400

    Added Mongodb atlas cluster

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
+const dbUrl = process.env.DB_URL
+// 'mongodb://localhost:27017/golfMap2021'
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
-
+ 
 app.use(express.static('public'))
 
 // Mongoose
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/golfMap2021', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() =>{
         console.log('CONNECTION')
     })
     .catch((err)=>{
         console.log('ERROR')
         console.log(err)
-    })
+    }) 
 
 
 // Require Golf Course Model 
