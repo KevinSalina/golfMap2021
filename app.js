65707194e4dab864aeb58bc525e9f985bcb2237f
Kevin Salina <kevin.salina25@gmail.com>
2021-04-29 15:01:47 -0400

    Updated npm start script and port for Heroku deployment

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get('/', async (req, res) =>{
 
 
 // Listen on Port 
-app.listen(3000, ()=>{
-    console.log('Listening on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`)
 })
