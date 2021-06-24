8cda1b5e9de4f95e66a5441f9c160b9ae8a72d3e
Kevin Salina <kevin.salina25@gmail.com>
2021-06-23 22:49:40 -0400

    Fixed JSON.stringify bug. Solution - disable auto format on save for .ejs files

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/golfMap2021'
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
- 
+
 app.use(express.static('public'))
 
 // Mongoose
 const mongoose = require('mongoose')
-mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() =>{
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => {
         console.log('CONNECTION')
     })
-    .catch((err)=>{
+    .catch((err) => {
         console.log('ERROR')
-        console.log(err) 
-    }) 
+        console.log(err)
+    })
 
 
 // Require Golf Course Model 
@@ -29,14 +29,14 @@ const golfCourseData = require('./utilities/golfCourseObject.js')
 
 
 // GeoCode get coordinates
-app.get('/', async (req, res) =>{
+app.get('/', async (req, res) => {
     const golfCourses = await GolfCourse.find({})
-    res.render('index', {golfCourses})
+    res.render('index', { golfCourses: JSON.stringify(golfCourses) })
 })
 
 
 // Listen on Port 
 const port = process.env.PORT || 3000
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
--- public/js/map.js
+++ public/js/map.js
@@ -5,41 +5,37 @@ const map = new mapboxgl.Map({
     style: 'mapbox://styles/kevinsalina/cko08i6lx5cop18o4igoxuojl', // style URL
     center: [-72.66145337909197, 41.74844854164804], // starting position [lng, lat]
     zoom: 7 // starting zoom
-    });
+});
 
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
-    for(course of golfCourses){
-        const el = document.createElement('div')
-        el.className = 'marker'
-        
-        const popup = new mapboxgl.Popup({offset: 25}).setHTML(
-            `<a class="popup-link" href="${course.website}" target="_blank" rel="noopener noreferrer">${course.name}</a>
-            <p class="popup-location"><b>${course.location}</b></p>
-            <p class="popup-desc">${course.description}</p>
-            <a class="popup-link" href="https://www.google.com/maps/dir//${course.name}/@${course.geometry.coordinates[1]}, ${course.geometry.coordinates[0]}}" target="_blank" rel="noopener noreferrer">Directions</a>`
-        )
-        new mapboxgl.Marker(el)
+for (course of golfCourses) {
+    const el = document.createElement('div')
+    el.className = 'marker'
+
+    const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+        `<a class="popup-link" href="${course.website}" target="_blank" rel="noopener noreferrer">${course.name}</a>
+        <p class="popup-location"><b>${course.location}</b></p>
+        <p class="popup-desc">${course.description}</p>
+        <a class="popup-link" href="https://www.google.com/maps/dir//${course.name}/@${course.geometry.coordinates[1]}, ${course.geometry.coordinates[0]}}" target="_blank" rel="noopener noreferrer">Directions</a>`
+    )
+    new mapboxgl.Marker(el)
         .setLngLat(course.geometry.coordinates)
         .setPopup(popup)
         .addTo(map)
-    }
+}
 
-    const layerList = document.querySelector('#menu')
-    const inputs = layerList.querySelectorAll('input')
+const layerList = document.querySelector('#menu')
+const inputs = layerList.querySelectorAll('input')
 
-    function switchLayer(layer){
-        const layerId = layer.target.id;
-        map.setStyle('mapbox://styles/' + layerId);
-    }
+function switchLayer(layer) {
+    const layerId = layer.target.id;
+    map.setStyle('mapbox://styles/' + layerId);
+}
 
-    inputs.forEach(input =>{
-        input.onclick = switchLayer;
-    })
-
-
-  
-        
+inputs.forEach(input => {
+    input.onclick = switchLayer;
+})
 
 
 
@@ -47,10 +43,13 @@ const map = new mapboxgl.Map({
 
 
 
- 
 
 
 
 
-   
- 
+
+
+
+
+
+
